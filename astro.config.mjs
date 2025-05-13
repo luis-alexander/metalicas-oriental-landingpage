244aeb8b9517574cfb1dace9fc4a39dfff0c3484
luis-alexander <luis.soto@sotsun.com>
2025-05-13 16:47:18 -0500

    Preparacion para deplegar en render 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://ALEXITOSOTO.github.io',
-    base: 'metalicas-oriental-landingpage',
+    output: 'static', 
 });
